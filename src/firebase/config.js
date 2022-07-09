015c2a9987b462470a4a9432874f7d4c63d6e6e7
Musratovf <musratovf@gmail.com>
2022-07-09 23:24:15 +0500

    Firebase and React combined

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+import React from 'react';
+const firebaseConfig = {
+	apiKey: 'AIzaSyBIPKK50NB8I9ikygniHQqz2Cf7e43MrL8',
+	authDomain: 'portfolio-website-a9017.firebaseapp.com',
+	projectId: 'portfolio-website-a9017',
+	storageBucket: 'portfolio-website-a9017.appspot.com',
+	messagingSenderId: '626717135293',
+	appId: '1:626717135293:web:1e6003d068d11e307102d4',
+	measurementId: 'G-7YKWNQK9KY',
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+export default db;
