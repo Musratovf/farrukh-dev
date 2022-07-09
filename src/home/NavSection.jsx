7e7477fccf5acc5bee8bfcfd0bc98f8de427f5e9
Musratovf <musratovf@gmail.com>
2022-07-09 21:59:06 +0500

    add fonts navbar

--- src/home/NavSection.jsx
+++ src/home/NavSection.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import '../styles/main.scss';
 import { Link } from 'react-router-dom';
 import { Collapse, Navbar, NavbarToggler, Nav, NavItem } from 'reactstrap';
 const NavSection = () => {
