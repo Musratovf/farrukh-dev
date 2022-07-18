c50e6b4fc1d76d5d0284c48a2908efb42ca50a54
Musratovf <musratovf@gmail.com>
2022-07-18 11:05:33 +0500

    firebase technology has been removed

--- src/firebase/Config.jsx
+++ src/firebase/Config.jsx
@@ -1,7 +1,6 @@
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import firebase from 'firebase';
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-
 const firebaseConfig = {
 	apiKey: 'AIzaSyBxDGu_a1QUVrXgZ6MCaS07BGNQ-Jd6_lw',
 	authDomain: 'portfolio-ffa0e.firebaseapp.com',
@@ -12,7 +11,27 @@ const firebaseConfig = {
 	measurementId: 'G-BLLTDMJJ9J',
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-export const auth = getAuth();
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+// export default db;
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore();
 export default db;
+// import { initializeApp } from 'firebase/app';
+// import { getAuth } from 'firebase/auth';
+// import { getFirestore } from 'firebase/firestore';
+
+// const firebaseConfig = {
+// 	apiKey: 'AIzaSyBxDGu_a1QUVrXgZ6MCaS07BGNQ-Jd6_lw',
+// 	authDomain: 'portfolio-ffa0e.firebaseapp.com',
+// 	projectId: 'portfolio-ffa0e',
+// 	storageBucket: 'portfolio-ffa0e.appspot.com',
+// 	messagingSenderId: '453928869617',
+// 	appId: '1:453928869617:web:802318e62f3b91b0f21350',
+// 	measurementId: 'G-BLLTDMJJ9J',
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+// export const auth = getAuth();
+// export default db;
--- src/home/AboutInfo.jsx
+++ src/home/AboutInfo.jsx
@@ -1,39 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import db from './../firebase/Config';
-import { addDoc, collection, doc, setDoc } from 'firebase/firestore';
+import React from 'react';
 
 const AboutInfo = () => {
-	const [portfolios, setPortfolios] = useState([]);
-
-	const handleAdd = async (e) => {
-		const res = await addDoc(collection(db, 'portfolio'), {
-			name: 'Los Angeles',
-			state: 'CA',
-			country: 'USA',
-		});
-		console.log(res.id);
-	};
-	// useEffect(() => {
-	// 	db.collection('portfolio').onSnapshot((snapshot) =>
-	// 		setPortfolios(snapshot.docs.map((doc) => doc.data()))
-	// 	);
-	// }, []);
-
-	return (
-		<div className='aboutInfo'>
-			{portfolios.map((portfolia) => {
-				<>
-					<div className='side1'>
-						<img src={portfolia.image} alt='me image' />
-					</div>
-					<div className='side2'>
-						<h1>{portfolia.title}</h1>
-						<p>{portfolia.description}</p>
-					</div>
-				</>;
-			})}
-		</div>
-	);
+	return <div>AboutInfo</div>;
 };
 
 export default AboutInfo;
