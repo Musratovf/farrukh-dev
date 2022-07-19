01b6a1d6fed5a17f33ef5165070f95190e9db6bf
Musratovf <musratovf@gmail.com>
2022-07-19 15:48:45 +0500

    pageInfo page added

--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import NavSection from '../home/NavSection';
+import PageInfo from './PageInfo';
 const Portfolio = () => {
 	return (
 		<>
 			<NavSection />
-			Portfolio
+			<PageInfo />
 		</>
 	);
 };
