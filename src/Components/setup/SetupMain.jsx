f2889845b29e00f0da6949b508e138d166e45246
Musratovf <musratovf@gmail.com>
2022-07-22 18:37:19 +0500

    added new mixins

--- src/Components/setup/SetupMain.jsx
+++ src/Components/setup/SetupMain.jsx
@@ -1,17 +1,67 @@
 import React from 'react';
 import SetupImage from '../imgs/setup.jpg';
 import { Container, Col, Row } from 'reactstrap';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 const SetupMain = () => {
 	return (
 		<div className='setupMain'>
 			{' '}
 			<div className='setupImage'>
-				<img src={SetupImage} alt='My Setup image' />
+				<img
+					width={300}
+					height={800}
+					src={SetupImage}
+					alt='My Setup image'
+				/>
 			</div>
 			<Container>
 				<Row>
 					<Col md='8'>
-						<h1>Setup</h1>
+						<div className='mySetup'>
+							<span className='laptopspaces'>
+								<ul>
+									<li>
+										<a href=''>
+											<h2 className='laptopspacesTitle'>
+												Hp Pavilion Laptop
+											</h2>
+											<ArrowUpwardIcon />
+										</a>
+									</li>
+									<li>Intel Core I7-11th Gen</li>
+									<li>16GB RAM (15.8 GB usable )</li>
+									<li>HDD 1TB</li>
+									<li>Display FHD 15.8</li>
+									<li></li>
+								</ul>
+							</span>
+							<span className='laptopspaces'>
+								<ul>
+									<li>
+										<a href=''>
+											<h2 className='laptopspacesTitle'>
+												Hp pavilion Anyware Plays
+												everywere
+											</h2>
+											<ArrowUpwardIcon />
+										</a>
+									</li>
+									<li>Intel Core I7-11th Gen</li>
+									<li>16GB RAM (15.8 GB usable )</li>
+									<li>HDD 1TB</li>
+									<li>Display FHD 15.8</li>
+									<li></li>
+								</ul>
+							</span>
+							<span className='keyboarSpaces'>
+								<ul>
+									<li>
+										<h2>Keyboard & Mouse</h2>
+									</li>
+									<li>Auido by</li>
+								</ul>
+							</span>
+						</div>
 					</Col>
 				</Row>
 			</Container>
